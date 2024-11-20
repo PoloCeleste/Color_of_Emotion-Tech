2fcac81ee16c7a06ff578aa15b2c0e23415ec4ee
PoloCeleste <voidness12@gmail.com>
2024-11-20 17:57:13 +0900

    merging tmdb api and selenium crawling

--- api/tmdb.py
+++ api/tmdb.py
@@ -1,8 +1,47 @@
-﻿import requests, json, os, io
+﻿import requests, json, os
 from dotenv import load_dotenv, find_dotenv
-from urllib.request import urlopen
 from Pylette import extract_colors
 import numpy as np
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+import requests
+
+options = Options()
+# options.add_argument("--headless")  # 129버전에선 --headless=old로 흰창 날릴 순 있으나 다른버전에서 문제 생길 수 있으므로 저 멀리 날려버림
+# options.add_argument("--window-position=-15400,-15400")  # 창이 뜨는 위치 변경. 4k나 8k에선 보일수도?
+options.add_argument("log-level=3")
+options.add_argument("lang=ko_KR")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Whale/3.27.254.15 Safari/537.36")
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation'])
+options.add_experimental_option("useAutomationExtension", False)
+service = Service(ChromeDriverManager().install())
+
+url = 'https://pedia.watcha.com/ko-KR'
+
+dr = webdriver.Chrome(service=service, options=options)
+dr.get(url)
+wait = WebDriverWait(dr, 5)
+act = ActionChains(dr)
+
+dr.implicitly_wait(5)
+
+try:
+    bt = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.hsDVweTz")))
+    bt.click()
+except:pass
+
+dr.implicitly_wait(5)
+
 load_dotenv(find_dotenv())
 TMDB = os.getenv('tmdb')
 
@@ -31,6 +70,61 @@ providers_url = "https://api.themoviedb.org/3/watch/providers/movie?language=ko-
 genres_url = "https://api.themoviedb.org/3/genre/movie/list?language=ko"
 de=['adult', 'backdrop_path', 'video', 'popularity', 'vote_count']
 set_provider=(8,119,337,356,97,350)
+
+
+def selenium_data(movie_name, movie_year):
+    search_box = wait.until(EC.visibility_of_element_located((By.NAME, "searchKeyword")))
+
+    search_box.send_keys(movie_name)
+    search_box.send_keys(Keys.ENTER)
+
+    sleep(1)
+
+    movie_box = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "v1F9TlrZ")))
+    movie_list = WebDriverWait(movie_box, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "listWrapper")))
+
+    movie = WebDriverWait(movie_list, 5).until(EC.visibility_of_element_located((By.XPATH, f"//a[@title='{movie_name}']//div[contains(text(), {movie_year})]")))
+    movie.click()
+
+    sleep(1)
+
+    contents_url = dr.current_url+'/comments'
+
+    picture=[]
+    try:
+        gal = wait.until(EC.visibility_of_element_located((By.XPATH, "//h2[text()='갤러리']//..//..")))
+        btn = WebDriverWait(gal, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'button')))
+
+        while True:
+            try:
+                btn.send_keys(Keys.ENTER)
+                sleep(0.1)
+            except:break
+
+        galleries = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'MfhUSmJK')))
+
+        for gallery in galleries:
+            pic = WebDriverWait(gallery, 5).until(EC.visibility_of_element_located((By.TAG_NAME, "div")))
+            pstyle = pic.get_attribute('style')
+            picture.append(pstyle.split('"')[-2])
+    except:print(movie_name, "None Picture")
+    
+    video_url = []
+    try:
+        videotag = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "OEje6csz")))
+        for video in videotag:
+            video_url.append(requests.get(video.get_attribute('href')).url)
+    except:print(movie_name, "None Video")
+
+    dr.get(contents_url)
+    sleep(1)
+    reviews=[]
+    try:
+        comments = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'eqSewv3p')))
+        for comment in comments:reviews.append(comment.text)
+    except:print(movie_name, "None Reviews")
+    
+    return picture, video_url, reviews
 
 
 # 공급자 데이터
@@ -101,6 +195,12 @@ def movie_data():
             if p: response['watch_providers']=list(p)
             del response['id']
             
+            picture_url, video_url, reviews=selenium_data(response['title'], response['release_date'].split('-')[0])
+            
+            response['picture_url'] = picture_url if picture_url else 'null'
+            response['video_url']=video_url if video_url else 'null'
+            response['reviews']=reviews if reviews else 'null'
+            
             # 포스터 색감 추출하기
             palette = extract_colors(image_url=response['poster_path'])
             poster_palette = list(map(lambda x:[x.rgb[0], x.rgb[1], x.rgb[2], x.freq], palette))
@@ -109,6 +209,7 @@ def movie_data():
         # 처리 완료된 페이지 내용 결과에 추가
         movie_results+=responses
     file_out('movies', movie_results)
+    dr.quit()
 
 # 완성된 데이터 json으로 저장
 def file_out(dataname, data):
